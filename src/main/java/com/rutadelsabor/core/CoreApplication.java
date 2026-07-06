bf6ce2bd4e9bbbee207f4126f36c90c7353cff1b
Ariel <ariellegua2204@gmail.com>
2026-07-06 15:26:49 -0500

    actualización de componentes alineada con la documentaci

--- src/main/java/com/rutadelsabor/core/CoreApplication.java
+++ src/main/java/com/rutadelsabor/core/CoreApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class CoreApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
 		SpringApplication.run(CoreApplication.class, args);
 	}
 
